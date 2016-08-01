91a6d4456135131a3365f722a21e3a8788f2a435
ivanzugnoni <zugnoni.ivan@gmail.com>
2016-08-01 00:36:41 +0000

    Fix tests for Python 3 one more time

--- twitter_api/utils.py
+++ twitter_api/utils.py
@@ -6,8 +6,7 @@ JSON_MIME_TYPE = 'application/json'
 
 
 def hash_to_md5(string):
-    import ipdb; ipdb.set_trace()
-    return md5(str(string).encode('utf-8')).hexdigest()
+    return md5(string.encode('utf-8')).hexdigest()
     
 
 def auth_only(f):
