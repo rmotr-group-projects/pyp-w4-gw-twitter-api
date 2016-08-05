0e0ddccb3113b8e78a238f059181dbe95f63fcb0
Christopher Pourier <corez92@gmail.com>
2016-08-05 17:58:17 +0000

    Cleaned up and comments added, not passing python 3

--- twitter_api/main.py
+++ twitter_api/main.py
@@ -1,10 +1,5 @@
-# -*- coding: utf-8 -*-
-
 import sqlite3
-
-from flask import Flask
-from flask import g
-from flask import request, Response, abort
+from flask import Flask, request, Response, abort, g
 import random
 from hashlib import md5
 import json
@@ -13,7 +8,6 @@ from datetime import datetime
 from dateutil.parser import parse
 
 app = Flask(__name__)
-
 
 def connect_db(db_name):
     return sqlite3.connect(db_name)
@@ -24,28 +18,34 @@ def before_request():
     g.db = connect_db(app.config['DATABASE'])
 
 
-# Login POST view ###################################################################
+# Login POST view ##############################################################
 @app.route('/login', methods=['POST'])
 @json_only
 def login():
-
+    
+    # Checks to see if 'username' & 'password' keys are in the request.json dictionary.
+    # Otherwise 404 error.
     data = request.json
     expected_request_params = ('username', 'password')
+    ## Check to see if all the elements in the list comprehension is True
     if not all([param in data for param in expected_request_params]):
-       abort(400)
+      abort(400)
     
     # Checks if user trying to login already has a token active
-    if not g.db.execute("SELECT user_id, username FROM auth JOIN user ON auth.user_id = user.id WHERE username=?;", (data['username'],)).fetchall():
+    # Maybe since we added this, we can make our own test case to see if it works?
+    if not g.db.execute("SELECT user_id, username FROM auth JOIN user ON \
+                        auth.user_id = user.id WHERE username=?;", (data['username'],)).fetchall():
         pass
     
     users = g.db.execute("SELECT username, password, id FROM user").fetchall()
     for user in users:
-        # Checks to see if username exists. Otherwise 404 error.
+        # Checks to see if username exists. 
+        # Otherwise 404 error if nothing is returned before the loop finishes.
         if data['username'] == user[0]: 
             # Checks to see if password is correct. Otherwise 401 error.
             if hash_function(data['password']) == user[1]:
-                # If login is succesful -> 201
-                # INSERT access token into access table
+                # If login is succesful return a status code of 201 along with an access token.
+                # INSERT access token into auth table
                 token = generate_token()
                 g.db.execute("INSERT INTO auth (access_token, user_id) VALUES (?, ?)", (token, user[2]))
                 g.db.commit()
@@ -55,50 +55,51 @@ def login():
                 abort(401)
     abort(404)
 
-# Logout POST view ###################################################################
+# Logout POST view #############################################################
 @app.route('/logout', methods=['POST'])
 def logout():
     data = request.json
+    # Checks to see if an access token was provided. Otherwise 401 error.
     if 'access_token' not in data:
         abort(401)
         
-    # If logout succesful, return 204
+    # If logout succesful, return status code 204
     g.db.execute("DELETE FROM auth WHERE access_token = ?;", (data['access_token'],))
     g.db.commit()
     
     return Response(status=204)
     
 
-# Profile GET view #################################################################
-@app.route('/profile/<username>', methods=['GET'])
+# Profile GET view #############################################################
+@app.route('/profile/<username>')
 def get_profile(username):
     # Get all data for username
-    connect_db.text_factory = str
 
     cursor = g.db.execute("SELECT id, username, first_name, last_name, birth_date \
-        FROM user WHERE username = ?;", (username,))
+                        FROM user WHERE username = ?;", (username,))
     query_user = cursor.fetchone()
-    if query_user:
     
+    # Checks to see if the username exists. Otherwise 404 error.
+    if query_user:
         cursor = g.db.execute("SELECT id, created, content \
-                                        FROM tweet WHERE user_id = ?;",(query_user[0],))
+                            FROM tweet WHERE user_id = ?;",(query_user[0],))
         query_tweets = cursor.fetchall()
         
         user_data = { 
-            'user_id': query_user[0],
-            'username': query_user[1],
-            'first_name': query_user[2],
-            'last_name': query_user[3],
-            'birth_date': query_user[4]
-        }
+                    'user_id': query_user[0],
+                    'username': query_user[1],
+                    'first_name': query_user[2],
+                    'last_name': query_user[3],
+                    'birth_date': query_user[4]
+                    }
         user_tweets = []
         for tweet in query_tweets:
             new_tweet = {
-                'id': tweet[0],
-                'text': tweet[2],
-                'date': parse(tweet[1]).isoformat(),
-                'uri': "/tweet/{}".format(tweet[0])
-            }
+                        'id': tweet[0],
+                        'text': tweet[2],
+                        'date': parse(tweet[1]).isoformat(),
+                        'uri': "/tweet/{}".format(tweet[0])
+                        }
             user_tweets.append(new_tweet)
         
         user_data['tweet'] = user_tweets
@@ -110,16 +111,17 @@ def get_profile(username):
         abort(404)
  
  
-# Profile POST view #################################################################
-# auth_only already checks if there is token in place
+# Profile POST view ############################################################
 @app.route('/profile', methods=['POST'])
 @auth_only
 def post_profile():
     data = request.json
-    expected_request_params = ('first_name', 'last_name', 'birth_date')
-    if not all([param in data for param in expected_request_params]):
+    # Checks to see if all params are provided for update. Otherwise 400 error.
+    expected_update_params = ('first_name', 'last_name', 'birth_date')
+    if not all([param in data for param in expected_update_params]):
         abort(400)
-        
+    
+    # Update profile information for user currently logged in.    
     user_id = active_user_id_from_token(data['access_token'])
     query_tuple = (data['first_name'], data['last_name'], data['birth_date'], user_id)
     g.db.execute('UPDATE user SET first_name=?,last_name=?, birth_date=? WHERE id=?;',query_tuple)
@@ -131,20 +133,17 @@ def post_profile():
 @app.route('/tweet/<tweet_id>', methods=['GET'])
 def get_tweet(tweet_id):
     tweet_id = (int(tweet_id),)
-    # Give me the username pertaining to a user_id from tweet table
+    # SELECT some columns from tables tweet and user
+    # WHERE the tweet id is the same as the route link tweet id
+    # JOIN the rows together based on user IDs from both tables
     joining = g.db.execute("SELECT tweet.id, tweet.content, tweet.created, user.username FROM tweet JOIN user ON \
                 tweet.user_id = user.id WHERE tweet.id=?;", tweet_id).fetchone()
-    # Query TWEET table and fetch the tweet with the ID from the route link
-    # cursor = g.db.execute("SELECT id, created, content, \
-    #                     user_id FROM tweet WHERE id = ?;",tweet_id)
     list_of_tweet_ids = g.db.execute("SELECT id FROM tweet;").fetchall()
-    print(tweet_id)
-    print("JUST FOR FUN:" + str(list_of_tweet_ids))
+    
+    # Checks to see if the route link tweet id was in the database. Otherwise 404 error.
     if tweet_id not in list_of_tweet_ids:
         abort(404)
-    # query_for_tweet = cursor.fetchone()
-   
-    print("THIS IS WHAT YOU NEED TO REMEMBER: " + str(joining))
+
     response_dict = {
                       "id": joining[0],
                       "content": joining[1],
@@ -152,29 +151,29 @@ def get_tweet(tweet_id):
                       "profile": "/profile/{}".format(joining[3]),
                       "uri": "/tweet/{}".format(joining[0])
                     }
+                    
     response_json = json.dumps(response_dict)
     return Response(response_json, status=200, mimetype='application/json')
     
     
-# Tweet POST view ###################################################################
-# auth_only already checks if there is token in place
+# Tweet POST view ##############################################################
 @app.route('/tweet', methods=['POST'])
 @auth_only
 def post_tweet():
     data = request.json
-    expected_request_params = ('content', 'access_token')
-    if not all([param in data for param in expected_request_params]):
+    # Checks to see if the post contains all fields needed. Otherwise 400 error.
+    # auth_only decorator checks for access token already.
+    if 'content' not in data:
         abort(400)
-        
     user_id = active_user_id_from_token(data['access_token'])
     query_tuple = (user_id, data['content'])
+    # Tweet ID and time posted is automatically added by SQLite3.
     g.db.execute("INSERT INTO tweet (user_id, content) VALUES (?, ?)", query_tuple)
     g.db.commit()
     return Response(status=201)
     
     
-# Tweet DELETE view ###################################################################
-# auth_only already checks if there is token in place
+# Tweet DELETE view ############################################################
 @app.route('/tweet/<tweet_id>', methods=['DELETE'])
 @auth_only
 def delete_tweet(tweet_id):
@@ -182,28 +181,21 @@ def delete_tweet(tweet_id):
     if 'access_token' not in data:
         abort(401)
         
-    # fetch user_id from tweet and compare with user_id from access_token
+    # Fetch user_id from table tweet.
+    # Compare it with user_id pertaining to posted access_token.
     user_id = active_user_id_from_token(data['access_token'])
     cursor = g.db.execute("SELECT user_id FROM tweet WHERE id = ?;", (tweet_id,))
     tweet = cursor.fetchone()
+    # Checks to see if the tweet exists based on the tweet_id by querying the db. Otherwise 404 error.
     if not tweet:
         abort(404)
+    # Checks to see if the tweet corresponds to the user trying to delete it. Otherwise 401 error.
     if tweet[0] != user_id:
         abort(401)
-        
+    # Delete the tweet if no abort has been invoked.    
     g.db.execute("DELETE FROM tweet WHERE id = ?;", (tweet_id,))
     g.db.commit()
     return Response(status=204)
-
-
-@app.errorhandler(404)
-def not_found(e):
-    return '', 404
-
-
-@app.errorhandler(401)
-def not_found(e):
-    return '', 401
 
 
 def hash_function(text):
@@ -216,14 +208,12 @@ def generate_token():
     a_token = md5(random_hexd.encode('utf-8')).hexdigest()
     return a_token
     
-    
-def action_query(query, subs):
-    g.db.execute(query, subs)
-    g.db.commit()
-    return
-
-
 # Returns a user id based on token given
 def active_user_id_from_token(token):
     user_obj = g.db.execute("SELECT user_id, access_token FROM auth WHERE access_token=?;", (token,)).fetchone()
     return user_obj[0]
+    
+# def action_query(query, subs):
+#     g.db.execute(query, subs)
+#     g.db.commit()
+#     return
