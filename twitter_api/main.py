e699a8a5c79f735f1e93f9d703024831648c1722
Christopher Pourier <corez92@gmail.com>
2016-08-05 08:11:17 +0000

    current version

--- twitter_api/main.py
+++ twitter_api/main.py
@@ -7,7 +7,8 @@ import random
 from hashlib import md5
 import json
 from .utils import auth_only, json_only
-
+from datetime import datetime
+from dateutil.parser import parse
 
 app = Flask(__name__)
 
@@ -82,45 +83,63 @@ def get_profile(username):
         query_tweets = cursor.fetchall()
         
         user_data = { 
-            'user_id': query_user[0].encode('utf-8'), 
-            'username': query_user[1].encode('utf-8'),
-            'first_name': query_user[2].encode('utf-8'),
-            'last_name': query_user[3].encode('utf-8'),
+            'user_id': query_user[0],
+            'username': query_user[1],
+            'first_name': query_user[2],
+            'last_name': query_user[3],
             'birth_date': query_user[4]
         }
         user_tweets = []
         for tweet in query_tweets:
+            dateISO = parse(tweet[1]).isoformat()
+
+            # print(dateISO)
+            # Expected Date: 2016-06-01T05:13:00
+            # Result = 2016-06-01 05:13:00
+            # dateiso = datetime.strptime(tweet[1],'%Y-%m-%d ').isoformat()
+            # print(tweet[1])
+            # print(type(tweet[1]))
+
             new_tweet = {
                 'id': tweet[0],
-                'text': tweet[2].encode('utf-8'),
-                'date': tweet[1],
+                'text': tweet[2],
+                'date': parse(tweet[1]).isoformat(),
                 'uri': "/tweet/{}".format(tweet[0])
             }
             user_tweets.append(new_tweet)
         
-        user_data['tweets'] = user_tweets
+        user_data['tweet'] = user_tweets
         user_data['tweet_count'] = len(user_tweets)
-
-        response_json = json.dumps(user_data, encoding='utf8')
+        
+        response_json = json.dumps(user_data)
         return Response(response_json, status=200, mimetype='application/json')
     else:
-        return Response(status=404)
+        abort(404)
  
  
 # Profile POST view #################################################################
-@app.route('/profile', methods=['POST'])  
+# auth_only already checks if there is token in place
+@app.route('/profile', methods=['POST'])
+@auth_only
 def post_profile():
-    
-    
-    
-    
-    return Response(status=404)
-    
+    data = request.json
+    update_param_check = ('first_name', 'last_name', 'birth_date')
+    if all([param in data for param in update_param_check]):
+        user_id = active_user_from_token(data['access_token'])
+        datetime_birthday = parse(data['birth_date'])
+        query_tuple = (data['first_name'], data['last_name'], datetime_birthday, user_id)
+        g.db.execute('UPDATE user SET first_name=?,last_name=?, birth_date=? WHERE id=?;',query_tuple)
+        g.db.commit()
+        return Response(status=201)
+    else:
+        return abort(400)
 
 # Tweet POST view ###################################################################
+# auth_only already checks if there is token in place
 @app.route('/tweet', methods=['POST'])
+@auth_only
 def tweet():
-    return Response(status=200)
+    return Response(status=200, mimetype='application/json')
 
 @app.errorhandler(404)
 def not_found(e):
@@ -136,5 +155,12 @@ def generate_token():
 def hash_function(text):
     return md5(text.encode('utf-8')).hexdigest()
     
+def action_query(query, subs):
+    g.db.execute(query, subs)
+    g.db.commit()
+    return
 
-    
+# Returns a user id based on token given
+def active_user_from_token(token):
+    user_obj = g.db.execute("SELECT user_id, access_token FROM auth WHERE access_token=?;", (token,)).fetchone()
+    return user_obj[1]
