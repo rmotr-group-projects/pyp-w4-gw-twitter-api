cdec9ad6c17e9304907a8d1690d7f71952032b68
Christopher Pourier <corez92@gmail.com>
2016-08-05 04:18:48 +0000

    created auth_only decorator

--- twitter_api/utils.py
+++ twitter_api/utils.py
@@ -1,15 +1,28 @@
 from functools import wraps
 from hashlib import md5
-from flask import request, abort
+from flask import request, abort, Flask, g
+import sqlite3
 
 JSON_MIME_TYPE = 'application/json'
+
+app = Flask(__name__)
+
+def connect_db(db_name):
+    return sqlite3.connect(db_name)
+
+@app.before_request
+def before_request():
+    g.db = connect_db(app.config['DATABASE'])
 
 # Checks request token
 def auth_only(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        # implement your logic here
-        return f(*args, **kwargs)
+        if 'access_token' in kwargs:
+            # Checks if token exists
+            if g.db.execute('SELECT access_token FROM auth WHERE access_token=?',(kwargs['access_token'])).fetchone():
+                return f(*args, **kwargs)
+        abort(401)
     return decorated_function
 
 # Make sure content is suppose to be JSON
