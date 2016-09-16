c9b9c140b0fe92fcaa534a5c5bec07fe1713cb24
santiagobasulto <santiago.basulto@gmail.com>
2016-09-16 17:34:27 +0000

    Login and logout completed but tests fail.

--- twitter_api/main.py
+++ twitter_api/main.py
@@ -1,12 +1,15 @@
 import sqlite3
+import string
+import random
 
-from flask import Flask
-from flask import g
+from flask import Flask, g, make_response, jsonify, request
 import json
-from .utils import JSON_MIME_TYPE
+from .utils import JSON_MIME_TYPE, md5
 
 app = Flask(__name__)
 
+def auth_token_generator(size = 6, chars = string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 def connect_db(db_name):
     return sqlite3.connect(db_name)
@@ -18,12 +21,48 @@ def before_request():
 
 @app.route('/login', methods=['POST'])
 def login():
-    pass
-
+    username = request.get_json()['username'].strip()
+    
+    query = """SELECT id, username, password
+        FROM user WHERE username = {};
+    """
+    cur = g.db.execute(query.format(username))
+    logged_in_profile = cur.fetchall
+    
+    password = request.get_json()['password']
+    
+    if len(password) == 0:
+        unauthorized('')
+    
+    md5_password = md5(password)
+    
+    if md5_password != logged_in_profile[0][2]:
+        unauthorized('')
+    else:
+        auth_token = auth_token_generator()
+        
+        query = '''INSERT INTO 'auth' ('user_id', 'access_token')
+                VALUES ('{0}', '{1}');
+        '''
+        g.db.execute(query.format(logged_in_profile[0][2]))
+        g.db.commit()
+        
+        auth_data = {
+            'access_token': auth_token
+        }
+        
+        return make_response(jsonify(auth_data), 201)
+    
+    
 @app.route('/logout', methods=['POST'])
 def logout():
-    pass
-
+    query = '''DELETE FROM auth
+            WHERE access_token = '{}'
+    '''
+    g.db.execute(query.format(request.json['access_token']))
+    g.db.commit()
+    
+    
 app.route('/profile')
 def update_profile():
     pass
@@ -36,20 +75,23 @@ def get_profile(username):
     cur = g.db.execute(profile_query.format(username))
     profile_data = cur.fetchall()
     
-    p_id, username, first_name, last_name, birth_date = profile_data
+    if profile_data is None:
+        not_found('')
+    
+    p_id, user_name, first_name, last_name, birth_date = profile_data
     profile_json_data = {
         "id": p_id,
-        "username": username,
+        "username": user_name,
         "first_name": first_name,
         "last_name": last_name,
-        "birth_date": birth_date # Need to convert this date to proper format
+        "birth_date": str(birth_date), # Need to convert this date to proper format
     }
     
     tweet_query = """SELECT id, content, created
         FROM tweet
         WHERE user_id = {};
     """
-    cur = g.db.execute(tweet_query.format(p_id))
+    cur = g.db.execute(tweet_query.format(profile_json_data['id']))
     tweet_data = cur.fetchall()
     
     tweets = []
@@ -58,7 +100,7 @@ def get_profile(username):
         tweet = {
             "id": t_id,
             "content": content,
-            "date": created, # Need to convert this date/time to proper format
+            "date": str(created), # Need to convert this date/time to proper format
             "uri": "/tweet/%s" % t_id
         }
         tweets.append(tweet)
@@ -66,7 +108,7 @@ def get_profile(username):
     profile_json_data['tweet'] = tweets
     profile_json_data['tweet_count'] = len(tweets)
     
-    return json.dumps(profile_json_data), 200, {'Content-Type': JSON_MIME_TYPE}
+    return make_response(jsonify(profile_json_data), 200)
 
     
 @app.route('/tweet/<tweet_id>')
@@ -87,13 +129,13 @@ def get_tweet(tweet_id):
     cursor = g.db.execute(query, {'tweet_id': tweet_id})
     tweet = cursor.fetchone()
     if tweet is None:
-        not_found(404)
+        not_found('')
 
     t_id, content, created, username = tweet
     data = {
         "id": t_id,
         "content": content,
-        "date": created, # Need to convert this date/time to proper format
+        "date": str(created), # Need to convert this date/time to proper format
         "profile": "/profile/%s" % username,
         "uri": "/tweet/%s" % t_id
     }
@@ -105,7 +147,11 @@ def get_tweet(tweet_id):
 def not_found(e):
     return '', 404
 
-
 @app.errorhandler(401)
 def unauthorized(e):
     return '', 401
+    
+@app.errorhandler(400)
+def bad_request(e):
+    return '', 400
+
