dd56d3a84329ba481f92ff2ad26a72acbd2813e4
nekatak <nekatak200@yahoo.gr>
2016-09-16 11:21:51 +0200

    8 from 24 tests no successful login

--- twitter_api/main.py
+++ twitter_api/main.py
@@ -1,7 +1,9 @@
 import sqlite3
+import json
+from flask import (Flask, g, request, abort)
+import random
+from .utils import md5
 
-from flask import Flask
-from flask import g
 
 app = Flask(__name__)
 
@@ -16,6 +18,69 @@ def before_request():
 
 
 # implement your views here
+
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.json or not 'username' in request.json or not 'password' in request.json:
+        abort(400)
+    username = request.json.get('username', "")
+    password = request.json.get('password', "")
+    check_user = username_password(username, password)
+    if not check_user[0]:
+        abort(404)
+    if not check_user[1]:
+        abort(401)
+    token = random.SystemRandom().random()
+    access_token={
+                "access_token": str(token),
+                }
+    user_id = convert_username_to_id(username)
+    insert_token(user_id, access_token['access_token'])
+    return json.dumps(access_token), 201
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    if not request.json or not 'access_token' in request.json:
+        abort(401)
+    token = request.json.get('access_token')
+    curs = g.db.execute('SELECT * FROM auth')
+    data = curs.fetchall()
+    for x in data:
+        if token in x:
+            id = x[0]
+    g.db.execute('DELETE FROM auth WHERE id=%s' % id)
+    g.db.commit()
+    #check
+    return '', 204
+
+#helper functions
+def username_password(username, password):
+    curs = g.db.execute('SELECT * FROM user')
+    data = curs.fetchall()
+    arr = []
+    for x in data:
+        if username in x:
+            arr.append(1)
+            if md5(password).hexdigest() in x:
+                arr.append(1)
+            else:
+                arr.append(0)
+    if not arr:
+        arr = [0, 0]
+    return arr
+
+def insert_token(user_id, token):
+    curs = g.db.execute('INSERT INTO auth (user_id, access_token) VALUES (%s, "%s")'%(user_id, token))
+    g.db.commit()
+    curs = g.db.execute('select * from auth')
+
+def convert_username_to_id(username):
+    users_cursor = g.db.execute('SELECT * FROM user')
+    users_data = users_cursor.fetchall()
+    for x in users_data:
+        if username in str(x):
+            return x[0]
+
 
 
 @app.errorhandler(404)
--- twitter_api/utils.py
+++ twitter_api/utils.py
@@ -1,4 +1,5 @@
 from functools import wraps
+from hashlib import md5 as hashing
 
 JSON_MIME_TYPE = 'application/json'
 
@@ -7,7 +8,8 @@ def md5(token):
     Returns an md5 hash of a token passed as a string, performing an internal 
     conversion of the token to bytes if run in Python 3
     """
-    pass
+    hashed_token = hashing(token.encode('utf-8'))
+    return hashed_token
 
 def auth_only(f):
     @wraps(f)
