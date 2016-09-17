282751c975fe1ce9f1db5cbdfb8dccf52b8fec61
Preston Rodriguez <prestonr83@gmail.com>
2016-09-17 06:45:15 +0000

    Final Commit

--- twitter_api/main.py
+++ twitter_api/main.py
@@ -1,7 +1,7 @@
 import sqlite3
 from datetime import datetime
 from flask import Flask
-from flask import (g, request, jsonify)
+from flask import (g, request, jsonify, make_response)
 from .utils import *
 import json
 import time
@@ -104,7 +104,7 @@ def profile_view(username):
     tweet_list = _tweet_dict(user_info['user_id'])
     user_info['tweets'] = tweet_list
     user_info['tweet_count'] = len(tweet_list)
-    return jsonify(**user_info), 200
+    return make_response(jsonify(user_info), 200)
 
 
 @app.route('/profile', methods=['POST'])
