4baf68a5d7c8dcf08847fb6431902699d737a3eb
Andrew Wanex <awanex2705@gmail.com>
2017-04-15 16:52:59 +0000

    Initial commit

--- twitter_api/main.py
+++ twitter_api/main.py
@@ -1,7 +1,9 @@
 import sqlite3
 
 from flask import Flask
-from flask import g
+from flask import g, json, request, jsonify, redirect
+from .utils import _md5
+from random import getrandbits
 
 app = Flask(__name__)
 
@@ -16,7 +18,55 @@ def before_request():
 
 
 # implement your views here
-
+@app.route('/login', methods=['POST'])
+def login():
+    next = request.args.get('next', '/')
+    data = request.get_json()
+    username = data['username']
+    try:
+        password = _md5(data['password'])
+    except KeyError:
+        return redirect(next, 400)
+    query = "SELECT id, username, password FROM user WHERE username=:username;"
+    cursor = g.db.execute(query,{'username':username})
+    user = cursor.fetchone()
+    if user:
+        if password != user[2]:
+            return redirect(next, 401)
+        token = str(getrandbits(32))
+        query = 'INSERT INTO auth ("user_id", "access_token") VALUES (:user_id, :access_token);'
+        params = {'user_id': user[0], 'access_token':token}
+        g.db.execute(query, params)
+        g.db.commit()
+        return jsonify(access_token=token), 201
+    
+    return redirect(next, 404)
+    
+    
+@app.route('/logout', methods=['POST'])
+def logout():
+    next = request.args.get('next', '/')
+    data = request.get_json()
+    query = "SELECT * FROM auth WHERE access_token=:access_token;"
+    try:
+        params = {'access_token': data['access_token']}
+    except KeyError:
+        return redirect(next,401)
+    cursor = g.db.execute(query, params)
+    user = cursor.fetchone()
+    if user:
+        g.db.execute("DELETE FROM auth WHERE access_token=:access_token;", (data['access_token'],))
+        g.db.commit()
+        return redirect(next, 204)
+    return redirect(next,401)
+    
+    
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    if request.method == 'POST':
+                
+        
+    if request.method == 'GET':
 
 @app.errorhandler(404)
 def not_found(e):
--- twitter_api/utils.py
+++ twitter_api/utils.py
@@ -1,18 +1,19 @@
 from functools import wraps
+from hashlib import md5
 
 JSON_MIME_TYPE = 'application/json'
 
-def md5(token):
+def _md5(token):
     """
     Returns an md5 hash of a token passed as a string, performing an internal 
     conversion of the token to bytes if run in Python 3
     """
-    pass
+    return md5(token.encode('utf-8')).hexdigest()
 
 def auth_only(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        # implement your logic here
+        # is user
         return f(*args, **kwargs)
     return decorated_function
 
@@ -20,6 +21,9 @@ def auth_only(f):
 def json_only(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        # implement your logic here
+        try:
+            json.loads(args, kwargs)
+        except ValueError:
+            return 
         return f(*args, **kwargs)
     return decorated_function
