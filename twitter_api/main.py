65c31f8ea81ab05b1071dedc582875f1f02a9099
David Bouwkamp <davidbouw@gmail.com>
2017-05-20 20:22:47 +0000

    Initial commit.

--- twitter_api/main.py
+++ twitter_api/main.py
@@ -1,6 +1,12 @@
 import sqlite3
+import json
+import random
+import string
+from twitter_api.utils import md5
 
+from flask import abort
 from flask import Flask
+from flask import request
 from flask import g
 
 app = Flask(__name__)
@@ -14,15 +20,220 @@ def connect_db(db_name):
 def before_request():
     g.db = connect_db(app.config['DATABASE'])
 
+def random_string():
+    return "".join(random.choice(string.letters + string.digits) for _ in range(20))
 
 # implement your views here
+@app.route('/login', methods=['POST'])
+def login():
+    # actually check that the user is in
+    # http://flask.pocoo.org/docs/0.12/api/#flask.Request.get_json
+    #data = json.loads(request.data)
+    data = request.get_json()
+    user_name = data['username']
+    cursor = g.db.cursor() # https://docs.python.org/3/library/sqlite3.html#sqlite3.Connection.execute
+    cursor.execute('SELECT id, password FROM user WHERE username=?', (user_name,))
+    result = cursor.fetchone()
+    
+    
+    if result is None:
+        # user_name wasn't found in the user db, so we should
+        # http://flask.pocoo.org/docs/0.12/api/#flask.abort
+        return abort(404) #no return
+    else:
+        try:
+            password_hash = md5(data['password']).hexdigest() 
+        except KeyError:
+            return abort(400)
+        if result[1] != password_hash:
+            return abort(401)
+        else:
+            # generate the token, return the token, add to auth db
+            access_token = random_string()
+            user_id = result[0]
+            g.db.execute('INSERT INTO auth (user_id, access_token) VALUES (?, ?)', [user_id, access_token])
+            g.db.commit()
+    return json.dumps({'access_token': access_token}), 201
 
+@app.route('/logout', methods=['POST'])
+def logout():
+    data = request.get_json()
+    try:
+        access_token = data['access_token']
+        g.db.execute('DELETE FROM auth WHERE access_token=?', [access_token])
+        g.db.commit()
+        return '', 204
+    except KeyError:
+        return abort(401)
+
+def parse_tweet(tweet_row, tweet_field='text'):
+    """
+    id INTEGER PRIMARY KEY autoincrement,
+    user_id INTEGER,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    content TEXT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES user(id)
+    """
+    tweet_id, user_id, created, content = tweet_row
+    uri_string = '/tweet/{}'.format(tweet_id)
+    
+    # created == "YYYY-MM-DD TTTTTT"
+    # created.split() == ["YYYY-MM-DD", "TTTTTT"]
+    # 
+    created_string = "T".join(created.split())
+    return {
+        'date':created_string,
+        'id': tweet_id,
+        tweet_field: content,
+        'uri': uri_string
+    }
+
+@app.route('/profile', methods=['POST'])
+def post_profile():
+        
+    data = request.get_json()
+    if data is None:
+        return abort(400)
+    try:
+        #validate that data is of a proper form
+        access_token = data['access_token'] #checks if data['access_token'] exists by assignment
+        if len(data) != 4:#check that all required fields are present
+            return abort(400)
+            
+        #verify that access_token is registered to db
+        cursor = g.db.execute('SELECT id FROM auth WHERE access_token=?', [access_token]) #moves cursor to auth record matching access_token
+        account_info = cursor.fetchone()
+        if not account_info:#if no matching auth record exists, abort
+            return abort(401)
+            
+   
+        
+        # UPDATE table_name
+        # SET column1 = value1, column2 = value2...., columnN = valueN
+        # WHERE [condition];
+        g.db.execute('UPDATE user SET first_name=?, last_name=?, birth_date=? WHERE id=?', [data['first_name'], data['last_name'], data['birth_date'], account_info[0]])
+        g.db.commit()
+        
+        return '', 201
+    except KeyError:
+        return abort(401)
+
+@app.route('/tweet/<tweet_num>', methods=['GET', 'DELETE'])
+def get_tweet(tweet_num):
+    if request.method == 'GET':
+        cursor = g.db.execute('SELECT * FROM tweet WHERE id=?', [tweet_num])
+        result = cursor.fetchone()
+        if result is None:
+            return abort(404)
+        else:
+            user_id = result[1]
+            cursor = g.db.execute('SELECT username FROM user WHERE id=?', [user_id])
+            user_name = cursor.fetchone()[0]
+            tweet_dict = parse_tweet(result, tweet_field='content')
+            tweet_dict['profile'] = "/profile/{}".format(user_name)
+            return json.dumps(tweet_dict), 200, {'Content-Type': 'application/json'}
+            
+    else:
+        data = request.get_json()
+        if data is None:
+            return abort(400)
+        
+        cursor = g.db.execute('SELECT * FROM tweet WHERE id=?', [tweet_num])
+        result = cursor.fetchone()
+        if result is None:
+            return abort(404)
+        else:
+            user_id = result[1]
+            cursor = g.db.execute('SELECT username FROM user WHERE id=?', [user_id])
+            user_name = cursor.fetchone()[0]
+            # does user_name's access_token match the access_token from the data
+            access_token = data['access_token'] #checks if data['access_token'] exists by assignment
+            
+            #verify that access_token is registered to db
+            cursor = g.db.execute('SELECT id FROM auth WHERE access_token=?', [access_token]) #moves cursor to auth record matching access_token
+            account_result = cursor.fetchone()
+            if account_result is None:
+                return abort(401)
+            account_info = account_result[0]
+            
+            if account_info != user_id:
+                return abort(401)
+            # UPDATE table_name
+            # SET column1 = value1, column2 = value2...., columnN = valueN
+            # WHERE [condition];
+            g.db.execute('DELETE FROM tweet WHERE id=?', [tweet_num])
+            g.db.commit()
+            return '', 204
+            
+
+@app.route('/tweet', methods=['POST'])
+def post_tweet():
+    data = request.get_json()
+    if data is None:
+        return abort(400)
+        
+    try:
+        #validate that data is of a proper form
+        access_token = data['access_token'] #checks if data['access_token'] exists by assignment
+        if len(data) != 2:#check that all required fields are present
+            return abort(400)
+            
+        #verify that access_token is registered to db
+        cursor = g.db.execute('SELECT id FROM auth WHERE access_token=?', [access_token]) #moves cursor to auth record matching access_token
+        account_result = cursor.fetchone()
+        if account_result is None:
+            return abort(401)
+        account_info = account_result[0]
+            
+        # UPDATE table_name
+        # SET column1 = value1, column2 = value2...., columnN = valueN
+        # WHERE [condition];
+        g.db.execute('INSERT INTO tweet (user_id, content) VALUES (?, ?)', [account_info, data['content']])
+        g.db.commit()
+        
+        return '', 201
+    except KeyError:
+        return abort(401)
+
+@app.route('/profile/<user_name>', methods=['GET'])
+def user_page(user_name):
+    cursor = g.db.execute('SELECT * FROM user WHERE username=?', [user_name])
+    user_data = cursor.fetchone()
+
+    if user_data is None:
+        return abort(404)
+    cursor = g.db.execute('SELECT * FROM tweet WHERE user_id=?', [user_data[0]])
+    tweet_data = [parse_tweet(tweet) for tweet in cursor.fetchall()]
+
+    user_id, username, _, first_name, last_name, birth_date = user_data
+    
+    
+    result_dict = {
+            'user_id': user_id,
+            'username': user_name,
+            'first_name': first_name,
+            'last_name': last_name,
+            'birth_date': birth_date,
+            'tweets': tweet_data,
+            'tweet_count': len(tweet_data),
+        }
+    
+    return json.dumps(result_dict), 200,  {'Content-Type':'application/json'}
+    
+        
 
 @app.errorhandler(404)
 def not_found(e):
+    '''
+    Client was able to communicate with a given server, 
+    but the server could not find what was requested.
+    '''
     return '', 404
 
 
 @app.errorhandler(401)
 def not_found(e):
+    '''
+    Unauthorized: Access is denied due to invalid credentials.
+    '''
     return '', 401
--- twitter_api/utils.py
+++ twitter_api/utils.py
@@ -1,4 +1,5 @@
 from functools import wraps
+import hashlib
 
 JSON_MIME_TYPE = 'application/json'
 
@@ -7,7 +8,9 @@ def md5(token):
     Returns an md5 hash of a token passed as a string, performing an internal 
     conversion of the token to bytes if run in Python 3
     """
-    pass
+    m = hashlib.md5()
+    m.update(token.encode('utf-8'))
+    return m #if it wants a raw binary instead of a hexadecimal string, use digest()
 
 def auth_only(f):
     @wraps(f)
