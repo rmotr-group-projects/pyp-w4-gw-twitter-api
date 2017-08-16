bf69e0812774d70d61cb8f3eff324276d3f0235e
Santiago Basulto <santiago.basulto@gmail.com>
2017-08-16 20:47:21 -0300

    Debug true

--- twitter_api/wsgi.py
+++ twitter_api/wsgi.py
@@ -4,6 +4,7 @@ from whitenoise import WhiteNoise
 from twitter_api.main import app
 
 app.debug = True
+app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
 app.config['DATABASE_URL'] = os.environ.get('DATABASE_URL', 'XYZ')
 
