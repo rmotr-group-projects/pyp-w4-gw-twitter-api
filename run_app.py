94b092630cc14a9892593009c8fb7a5db1175942
Santiago Basulto <santiago.basulto@gmail.com>
2017-08-16 21:00:06 -0300

    Debug true

--- run_app.py
+++ run_app.py
@@ -6,10 +6,10 @@ from twitter_api.main import app
 if __name__ == '__main__':
     app.debug = True
 
-    # host = os.environ.get('IP', '0.0.0.0')
-    # port = int(os.environ.get('PORT', 8080))
-    # app.run(host=host, port=port)
     app.config['DEBUG'] = True
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
     app.config['DATABASE_URL'] = os.environ.get('DATABASE_URL', 'XYZ')
-    app.run()
+
+    host = os.environ.get('IP', '127.0.0.1')
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host=host, port=port)
