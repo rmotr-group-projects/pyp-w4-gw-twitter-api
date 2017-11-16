1732787409cf7428759f91b3268347c4c44b952c
Khristopher Chireno <kcmastermind2005@gmail.com>
2017-11-16 11:55:10 +0000

    encoded token with utf-8

--- twitter_api/utils.py
+++ twitter_api/utils.py
@@ -9,7 +9,7 @@ def md5(token):
     Returns an md5 hash of a token passed as a string, performing an internal 
     conversion of the token to bytes if run in Python 3
     """
-    return hashlib.md5(token)
+    return hashlib.md5(token.encode('utf-8'))
 
 def auth_only(f):
     @wraps(f)
