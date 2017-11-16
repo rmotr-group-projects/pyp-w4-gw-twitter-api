69cbd5e651842ce2c1995dac10d53390ebb6d900
Khristopher Chireno <kcmastermind2005@gmail.com>
2017-11-16 12:06:19 +0000

    fixed utf-8 for bytes

--- twitter_api/utils.py
+++ twitter_api/utils.py
@@ -4,20 +4,22 @@ from flask import request, abort, g
 
 JSON_MIME_TYPE = 'application/json'
 
+
 def md5(token):
     """
-    Returns an md5 hash of a token passed as a string, performing an internal 
+    Returns an md5 hash of a token passed as a string, performing an internal
     conversion of the token to bytes if run in Python 3
     """
-    return hashlib.md5(token.encode('utf-8'))
+    return hashlib.md5(bytes(token, 'utf-8'))
+
 
 def auth_only(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         # implement your logic here
-        if request.method =='GET':
+        if request.method == 'GET':
             return f(*args, **kwargs)
-        elif request.method in ['DELETE','POST']:
+        elif request.method in ['DELETE', 'POST']:
             if 'access_token' not in request.json:
                 abort(401)
             return f(*args, **kwargs)
