3a227e1e1f242704210ac72ca76052492b1219d7
Palermo Penano <palermo.penano@mail.utoronto.ca>
2017-12-24 15:10:28 -0500

    md5 token

--- twitter_api/utils.py
+++ twitter_api/utils.py
@@ -1,13 +1,21 @@
+import hashlib
 from functools import wraps
 
 JSON_MIME_TYPE = 'application/json'
 
+
 def md5(token):
     """
-    Returns an md5 hash of a token passed as a string, performing an internal 
+    Returns an md5 hash of a token passed as a string, performing an internal
     conversion of the token to bytes if run in Python 3
+
+    What is going on here ???
     """
-    pass
+    new_token = token
+    if str != bytes:
+        new_token = token.encode('utf-8')
+    return hashlib.md5(new_token)
+
 
 def auth_only(f):
     @wraps(f)
