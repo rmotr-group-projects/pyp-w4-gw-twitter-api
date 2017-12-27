d6805bc0b4ffeb891afbbdae27a2c7a3755d28bf
Palermo Penano <palermo.penano@mail.utoronto.ca>
2017-12-26 19:29:23 -0500

    implement auth_only

--- twitter_api/utils.py
+++ twitter_api/utils.py
@@ -1,5 +1,7 @@
 import hashlib
 from functools import wraps
+import datetime
+from flask import make_response, request, abort, g
 
 JSON_MIME_TYPE = 'application/json'
 
@@ -17,17 +19,59 @@ def md5(token):
     return hashlib.md5(new_token)
 
 
+def sqlite_date_to_python(date_str):
+    return datetime.datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+
+
+def python_date_to_json_str(dt):
+    return dt.strftime("%Y-%m-%dT%H:%M:%S")
+
+
 def auth_only(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        # implement your logic here
+
+        if 'access_token' not in request.json:
+            abort(401)
+
+        # get user_id that matches access token from auth database
+        query = """
+            SELECT user_id FROM auth
+            WHERE access_token=:access_token;
+        """
+        param = {'access_token': request.json['access_token']}
+        cursor = g.db.execute(query, param)
+        user_id = cursor.fetchone()
+
+        # check access token provided in **kwargs through the post request
+        # abort with unauthorized request status code if user with given
+        # access token does not exist
+        if not user_id:
+            abort(401)
+
+        # return as user id
+        kwargs['user_id'] = user_id[0]
+
         return f(*args, **kwargs)
     return decorated_function
 
 
 def json_only(f):
+    """Check if data provided is in json format"""
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        # implement your logic here
+
+        if request.content_type != JSON_MIME_TYPE:
+            abort(400)
+
         return f(*args, **kwargs)
+
     return decorated_function
+
+
+def json_response(data='', status=200, headers=None):
+    headers = headers or {}
+    if 'Content-Type' not in headers:
+        headers['Content-Type'] = JSON_MIME_TYPE
+
+    return make_response(data, status, headers)
